17f856cacd4f2b1b78511c2b35176d4989ad57f4
Celine Smith <cnsmith13@alaska.edu>
2026-04-12 06:55:08 +0900

    fixed errors in script, now it is functioning

--- roses_and_maps.py
+++ roses_and_maps.py
@@ -6,34 +6,43 @@ import pygmt
 '''
 This script reads in a csv of lineations with line bearings between 0-180, and
 corner coordinates of a bounding box, and creates a rose diagram using PyGMT.
+Coming soon: makes an accompanying map that plots the points of the lineations
+in the specified bounding box.
 
 input: 
 csv filepath
-bounding box corner coordinates (not yet done)
+(coming soon) bounding box corner coordinates
 
 output:
-rose diagram
+rose diagram in pdf form
 
 exceptions:
--csv must have x, y coordinates and line bearing columns labelled as:
+-.csv must have x, y coordinates and line bearing columns labelled as:
 x_coord, y_coord, bearing_corrected
--x_coords and y_coords must be in decimal degrees (not yet done)'''
+-(coming soon) x_coords and y_coords must be in decimal degrees (not yet done)
+'''
 
-print("is this working")
+print("this is working")
 
-import pygmt
 
-print("is this working")
+#asking user for file and reading it
+csv_file = input("Enter full path to CSV file: ").strip()
+df = pd.read_csv(csv_file)
 
-azimuths = [90, 90, 75, 60]
+#extracting line bearings into a dataframe
+azimuths = df["bearing_corrected"].dropna()
 lengths = [1] * len(azimuths) 
 
+
+#variables to adjust
 bin_width = 10
-r_max = 15
-radial_grid = "xg5"
+r_max = 250
+radial_grid = "xg20"
 azimuth_grid = "yg30"
 color = "green"
 
+
+#PYGMT
 fig = pygmt.Figure()
 
 fig.rose(
