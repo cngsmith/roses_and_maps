b60047256fbb89dca643797231645c7d3bf07a96
Celine Smith <cnsmith13@alaska.edu>
2026-04-12 06:04:38 +0900

    working script with toy data

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # roses_and_maps
 A program to create rose diagrams and maps using PyGMT, for mega-scale glacial lineation interpretation.
 
-##installation instructions
+installation instructions:
 
-install PyGMT and it's associated dependencies to your conda/mamba environment
+install PyGMT and it's associated dependencies to your conda/mamba environment.
     for example:
     
     >micromamba activate GEOS694
--- roses_and_maps.py
+++ roses_and_maps.py
@@ -2,75 +2,49 @@ import pandas as pd
 import numpy as np
 import pygmt
 
-pygmt.show_versions()
+
+'''
+This script reads in a csv of lineations with line bearings between 0-180, and
+corner coordinates of a bounding box, and creates a rose diagram using PyGMT.
+
+input: 
+csv filepath
+bounding box corner coordinates (not yet done)
+
+output:
+rose diagram
+
+exceptions:
+-csv must have x, y coordinates and line bearing columns labelled as:
+x_coord, y_coord, bearing_corrected
+-x_coords and y_coords must be in decimal degrees (not yet done)'''
+
 print("is this working")
 
+import pygmt
 
+print("is this working")
 
+azimuths = [90, 90, 75, 60]
+lengths = [1] * len(azimuths) 
 
+bin_width = 10
+r_max = 15
+radial_grid = "xg5"
+azimuth_grid = "yg30"
+color = "green"
 
-#install packages
+fig = pygmt.Figure()
 
+fig.rose(
+    length=lengths,
+    azimuth=azimuths,
+    region=[0, r_max, 0, 360],
+    sector=bin_width,
+    diameter="15c",
+    fill=color,
+    pen="1p,black",
+    frame=[radial_grid, azimuth_grid],
+)
 
-#m
-
-
-
-
-
-
-
-
-# ##PAG CODE
-
-
-# ##reading my attribute .csv as a dataframe
-# df = pd.read_csv("indiv_orientations_msgl.csv")
-
-# ##defining columns
-# az_col = "azimuth_corrected"
-# ice_col = "ice_flow_provenance"
-# ref_col = "smith_reference" 
-# cruise_col = "smith_cruise"
-
-# ##filtering csv
-# # filtering by content, comment row out if you don't want to filter by it, or make a list for multiple
-# selected_ice = ["Chukchi Shelf"]
-# #selected_ref = ["Smith"]
-# #selected_cruise = ["Sikuliaq_2021"]
-
-# #creating a filtered dataframe by creating a boolean series according to my chosen filter, comment out line you don't need
-# df_filtered = df.copy()
-
-# df_filtered = df_filtered[df_filtered[ice_col].isin(selected_ice)]
-# #df_filtered = df_filtered[df_filtered[ref_col].isin(selected_ref)]
-# #df_filtered = df_filtered[df_filtered[cruise_col].isin(selected_cruise)]
-
-# msgl = df_filtered[az_col]
-
-# #----------------------------------
-
-# ##Pygmt
-
-# #variables to change 
-# bin_width = 10
-# r_max = 15
-# radial_grid = "xg5"
-# azimuth_grid = "yg30"
-# color = "green"
-
-# #begin plotting
-
-# fig = pygmt.Figure()
-# fig.rose(
-#     data=msgl,   
-#     incols=[0],
-#     region=[0, r_max, 0, 360],
-#     sector=bin_width,
-#     diameter="15c",
-#     fill= color,
-#     pen="1p,black",
-#     frame=[radial_grid, azimuth_grid],
-# )
-
-# fig.show()
+fig.show()
