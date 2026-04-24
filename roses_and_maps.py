25fd34af66bd8d05a6a54c784d4520700efff551
Celine Smith <cnsmith13@alaska.edu>
2026-04-23 17:26:17 -0800

    restructuring script to implement classes

--- roses_and_maps.py
+++ roses_and_maps.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import pygmt
 
 
@@ -25,35 +24,75 @@ x_coord, y_coord, bearing_corrected
 print("this is working")
 
 
-#asking user for file and reading it
-csv_file = input("Enter full path to CSV file: ").strip()
-df = pd.read_csv(csv_file)
+class RosesAndMaps:
+    def __init__(self, csv_path,):
+        '''
+        initializes the instance by reading in the data and setting up
+        a copy for filtering
+        '''
+        #create a pandas dataframe of lineations in the csv file
+        self.df = pd.read_csv(csv_path)
+        #creating a copy of data from which to filter
+        self.filtered_df = self.df.copy()
 
-#extracting line bearings into a dataframe
-azimuths = df["bearing_corrected"].dropna()
-lengths = [1] * len(azimuths) 
-
-
-#variables to adjust
-bin_width = 10
-r_max = 250
-radial_grid = "xg20"
-azimuth_grid = "yg30"
-color = "green"
+        #debugging check
+        print(f"Successfully loaded {len(self.df)} lineations.")
 
 
-#PYGMT
-fig = pygmt.Figure()
+    # def set_bbox(self, x1, x2, y1, y2):
+    # def plot_rose(self, output_filename):
+    # def plot_map(self, output_filename):
 
-fig.rose(
-    length=lengths,
-    azimuth=azimuths,
-    region=[0, r_max, 0, 360],
-    sector=bin_width,
-    diameter="15c",
-    fill=color,
-    pen="1p,black",
-    frame=[radial_grid, azimuth_grid],
-)
 
-fig.show()
+if __name__ == "__main__":
+    csv_path = input("Enter full path to CSV file: ").strip()
+    
+    # Initialize the instance
+    my_lineations = RosesAndMaps(csv_path)
+ 
+
+# ##asking for user input: data file path and map bounding box
+# csv_file = input("Enter full path to CSV file: ").strip()
+# df = pd.read_csv(csv_file)
+
+# x_min = input("Enter the x_coordinate for the top left corner of the map: ").strip()
+# y_max = input("Enter the y_coordinate for the top left corner of the map: ").strip()
+# x_max = input("Enter the x_coordinate for the bottom right corner of the map: ").strip()
+# y_min = input("Enter the y_coordinate for the bottom right corner of the map: ").strip()
+
+
+
+
+
+# #asking user for file and reading it
+# csv_file = input("Enter full path to CSV file: ").strip()
+# df = pd.read_csv(csv_file)
+
+# #extracting line bearings into a dataframe
+# azimuths = df["bearing_corrected"].dropna()
+# lengths = [1] * len(azimuths) 
+
+
+# #variables to adjust
+# bin_width = 10
+# r_max = 250
+# radial_grid = "xg20"
+# azimuth_grid = "yg30"
+# color = "green"
+
+
+# #PYGMT
+# fig = pygmt.Figure()
+
+# fig.rose(
+#     length=lengths,
+#     azimuth=azimuths,
+#     region=[0, r_max, 0, 360],
+#     sector=bin_width,
+#     diameter="15c",
+#     fill=color,
+#     pen="1p,black",
+#     frame=[radial_grid, azimuth_grid],
+# )
+
+# fig.show()
